5d8095c03970402293886de3bd94cdc09473e8eb
Atlant5000 <maximmmd13@gmail.com>
2024-11-16 17:26:33 +0200

    Task 3

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Value();
+        }
+        public static void Value()
+        {
+            Console.Write("a=");
+            double a = double.Parse(Console.ReadLine());
+            double result = Function(a);
+            Console.WriteLine($"b={result}");
+            Console.Read();
+        }
+        public static double Function(double a)
+        {
+            return 12 * Math.Pow(a, 2) + 7 * a - 6;
+        }
+    }
+}
