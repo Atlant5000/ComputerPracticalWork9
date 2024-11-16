ba0e5183d57f3c9189050c157c2d0fb72b4f5231
Atlant5000 <maximmmd13@gmail.com>
2024-11-16 15:45:01 +0200

    Task 1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sayhello();
+            Saybye();
+        }
+        public static void Sayhello() 
+        {
+            Console.WriteLine("Привет.");
+        }
+        public static void Saybye()
+        {
+            Console.WriteLine("Пока.");
+            Console.Read();
+        }
+    }
+}
