1c12940cbfa9ec56d5039fcfa5da61a295243c23
Atlant5000 <maximmmd13@gmail.com>
2024-11-16 16:23:42 +0200

    Task 2

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Value();
+        }
+        public static void Value() 
+        {
+            Console.Write("a=");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b=");
+            int b = int.Parse(Console.ReadLine());
+            int result = Sum(a, b);
+            Console.WriteLine($"Sum={result}");
+            Console.Read();
+        }
+        public static int Sum(int a,int b)
+        {
+            return a + b;
+        }
+    }
+}
